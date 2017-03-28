0c79611dd6b1b3f4f8c426441ed9d38653c7a738
Pranit Bauva <pranit.bauva@gmail.com>
2017-03-28 15:04:42 +0530

    read and display images
    Signed-off-by: Pranit Bauva <pranit.bauva@gmail.com>

--- main.cpp
+++ main.cpp
@@ -4,5 +4,21 @@ using namespace std;
 using namespace cv;
 
 int main() {
+   char *debug = getenv("DEBUG");
+
+	// Read the images as grayscale
+	Mat left = imread("left.jpg", 0);
+	Mat right = imread("right.jpg", 0);
+
+	// Show the images
+	if (debug) {
+		namedWindow("Left", WINDOW_AUTOSIZE);
+		namedWindow("Right", WINDOW_AUTOSIZE);
+		imshow("Left", left);
+		imshow("Right", right);
+	}
+
+
+	waitKey(0);
 	return 0;
 }
